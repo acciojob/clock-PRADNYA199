a1ff8fac3f92b1a33b45dabe5a54346f19b60acb
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-10-07 22:23:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timeDisplay=document.getElementById("timer")
+lat currentTime=new Date();
+setInterval(
+	function(){
+		currentTime=new Date();
+		timeDisplay.innerHTML=currentTimeToLocalString()
+		
+		
+	}
+)
